0c13ccbed6ececd3d73a0917ce6f91819af6d79a
Wilson <wphoong@gmail.com>
2018-03-23 14:46:36 -0700

    adjusted port to include .env.PORT

--- server/server.js
+++ server/server.js
@@ -22,7 +22,7 @@ app.get("*", (req, res) => {
 });
 
 // LISTEN SERVER
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 io.on('connection', (socket) => {
 	console.log("connected to stock price app");
